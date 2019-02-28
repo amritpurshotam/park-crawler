99e2ae932f8e0b0ade3fcab4557989bd0ba5ac9b
Amrit Purshotam <amrit.purshotam@entelect.co.za>
2019-02-28 13:34:16 +0200

    Get list of events in South Africa

---
+++ getevents.py
@@ -0,0 +1,22 @@
+from request import get
+import xml.etree.ElementTree as ET
+
+result = get("https://www.parkrun.co.za/wp-content/themes/parkrun/xml/geo.xml")
+tree = ET.parse(result)
+
+region_arr = []
+
+for country in tree.getroot()[0]:
+    if (country.attrib['n'] == 'South Africa'):
+        for region in country:
+            region_arr.append(region.attrib['id'])
+
+index = 0
+for region in tree.getroot():
+    if index == 0:
+        index = index + 1
+        continue
+    else:
+        if region.attrib['r'] in region_arr:
+            print("{}\t{}\t{}\t{}".format(region.attrib['n'], region.attrib['m'], region.attrib['la'], region.attrib['lo']))
+        index = index + 1
---
+++ request.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+def get(url: str):
+    req = urllib.request.Request(
+        url, 
+        data=None, 
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        }
+    )
+
+    result = urllib.request.urlopen(req)
+    return result
