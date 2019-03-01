3cb96d2a076add72d17508a37745e9f99e0c7d28
Amrit Purshotam <amrit.purshotam@entelect.co.za>
2019-03-01 07:40:05 +0200

    Move model files to own folder and cleanup get events script

--- getevents.py
+++ getevents.py
@@ -1,35 +1,51 @@
 from request import get
 import xml.etree.ElementTree as ET
-from Country import Country
-from Region import Region
-from Course import Course
+from models.Country import Country
+from models.Region import Region
+from models.Course import Course
 from db import get_session, add, commit
+
+def add_country(country_dict):
+    id = country_dict['id']
+    name = country_dict['n']
+    base_url = country_dict['u']
+    latitude = country_dict['la']
+    longitude = country_dict['lo']
+    country = Country(id=id, name=name, base_url=base_url, latitude=latitude, longitude=longitude)
+    add(country, session)
+
+def add_region(region_dict):
+    id = region_dict['id']
+    country_id = region_dict['pid']
+    name = region_dict['n']
+    latitude = region_dict['la']
+    longitude = region_dict['lo']
+    region = Region(id=id, country_id=country_id, name=name, latitude=latitude, longitude=longitude)
+    add(region, session)
+
+def add_course(course_dict):
+    id = course_dict['id']
+    region_id = course_dict['r']
+    url = "{}/{}".format('https://www.parkrun.co.za', course_dict['n'])
+    name = course_dict['m']
+    latitude = course_dict['la']
+    longitude = course_dict['lo']
+    course = Course(id=id, region_id=region_id, name=name, url=url, latitude=latitude, longitude=longitude)
+    add(course, session)
 
 result = get("https://www.parkrun.co.za/wp-content/themes/parkrun/xml/geo.xml")
 tree = ET.parse(result)
 
 region_arr = []
 session = get_session()
-base_url = ''
 
 for country in tree.getroot()[0]:
     if (country.attrib['n'] == 'South Africa'):
-        id = country.attrib['id']
-        name = country.attrib['n']
-        base_url = country.attrib['u']
-        latitude = country.attrib['la']
-        longitude = country.attrib['lo']
-        c = Country(id=id, name=name, base_url=base_url, latitude=latitude, longitude=longitude)
-        add(c, session)
+        add_country(country.attrib)
         for region in country:
-            id = region.attrib['id']
-            country_id = c.id
-            name = region.attrib['n']
-            latitude = region.attrib['la']
-            longitude = region.attrib['lo']
-            r = Region(id=id, country_id=country_id, name=name, latitude=latitude, longitude=longitude)
-            add(r, session)
+            add_region(region.attrib)
             region_arr.append(region.attrib['id'])
+        break
     
 commit(session)   
 
@@ -40,15 +56,7 @@ for event in tree.getroot():
         continue
     else:
         if event.attrib['r'] in region_arr:
-            id = event.attrib['id']
-            region_id = event.attrib['r']
-            url = "{}/{}".format(base_url, event.attrib['n'])
-            name = event.attrib['m']
-            latitude = event.attrib['la']
-            longitude = event.attrib['lo']
-            course = Course(id=id, region_id=region_id, name=name, url=url, latitude=latitude, longitude=longitude)
-            add(course, session)
+            add_course(event.attrib)
         index = index + 1
-
 
 commit(session)
--- models/Country.py
+++ models/Country.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
