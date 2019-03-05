4b9e3c240c72ebf6fbf7ea0a9407b5cbfcbd7191
donne.stevenson <donne.stevenson@entelect.co.za>
2019-03-05 14:39:53 +0200

    Get last event for each course

--- data/repository/event.py
+++ data/repository/event.py
@@ -1,7 +1,22 @@
 from data.db import get_session
 from models.Event import Event
+from datetime import datetime
 
-def load_by_course(course_id):
+def get_by_course(course_id):
     sess = get_session()
     events = sess.query(Event).filter_by(course_id=course_id).all()
     return events
+
+def get_last_event_number(course_id):
+    sess = get_session()
+    events = sess.query(Event.run_sequence_number).filter_by(course_id=course_id).all()
+    return max(events)[0]
+
+def get_last_event_date(course_id):
+    events = get_by_course(course_id)
+    dates = [datetime.strptime(event.date, '%d/%m/%Y' ) for event in events]
+    if len(dates) == 0:
+        return 0
+    elif len(dates) > 0:
+        return max(dates)
+
