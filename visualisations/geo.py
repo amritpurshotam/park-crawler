529b880e7bac18397d126c21d8ed1c9f1ed03526
Amrit Purshotam <amrit.purshotam@entelect.co.za>
2019-03-05 09:50:44 +0200

    Show parkruns by region on map

--- visualisations/geo.py
+++ visualisations/geo.py
@@ -1,24 +1,34 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from models.Course import Course
-from data.db import load_all
+from models.Region import Region
+from data.repository.course import get_by_region
+from data.db import load_all_ids
 import numpy as np
 import random
 
 def show_parkrun_locations():
-    courses = load_all(Course)
-    latitudes = list(map(lambda course: course.latitude, courses))
-    longitudes = list(map(lambda course: course.longitude, courses))
-    sizes = []
-    ages = []
-    for i in range(0, len(latitudes)):
-        sizes.append(random.randint(1, 2000) / 20)
-        ages.append(random.randint(1, 400) / 4)
-
-
+    plt.figure(figsize=(40,40))
     m = Basemap(projection='lcc', resolution='f', lat_0=-29.202114, lon_0=24.147949, width=2000000, height=1.6E6)
-    m.shadedrelief()
     m.drawcountries()
-    x, y = m(longitudes, latitudes)
-    m.scatter(x, y, marker='.', c=np.log10(ages), s=sizes, cmap='Greens', alpha=0.5)
+    m.drawcoastlines()
+    m.readshapefile('visualisations/shapefiles/District_Municipalities_2016', 'provinces')
+
+    region_ids = load_all_ids(Region)
+    for region_id in region_ids:
+        courses = get_by_region(region_id)
+        latitudes = list(map(lambda course: course.latitude, courses))
+        longitudes = list(map(lambda course: course.longitude, courses))
+
+        sizes = []
+        ages = []
+        r = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
+        color = []
+        for i in range(0, len(latitudes)):
+            sizes.append(300)
+            color.append(r)
+
+        x, y = m(longitudes, latitudes)
+        m.scatter(x, y, marker='.', c=color, s=sizes, alpha=0.5)
+
     plt.savefig('visualisations/parkruns.png')
