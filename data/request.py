da9123fb0f3cef7de261ae86b785f370220ebbfc
donne.stevenson <donne.stevenson@entelect.co.za>
2019-03-05 14:40:43 +0200

    Randomize IP and user agents for requests

--- data/request.py
+++ data/request.py
@@ -1,15 +1,21 @@
 import urllib.request
+import requests
 import time
+import random
 
 def get(url: str):
-    req = urllib.request.Request(
-        url, 
-        data=None, 
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-        }
-    )
 
-    result = urllib.request.urlopen(req)
+    dummy_user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36",
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/605.1 Edge/19.17763"
+    ]
+
+    proxies = ['http://105.247.67.35:8080', 'http://105.255.223.2:8080', 'http://178.62.29.166:8080','http://138.68.165.154:8080']
+
+    req = requests.get(url, headers={'user-agent': random.choice(dummy_user_agents)}, proxies = {'http': random.choice(proxies)})
+
+    result = req.text
     time.sleep(1)
     return result
